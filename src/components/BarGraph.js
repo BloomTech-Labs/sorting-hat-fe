decfa85848d47819244bfbe8b387361022a5f547
Braden <fish2355@hotmail.com>
2020-04-06 15:10:42 -0600

    bar graph--y-axis and percents on bars

--- src/components/BarGraph.js
+++ src/components/BarGraph.js
@@ -1,84 +1,46 @@
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend
-} from "recharts";
 
 function BarGraph({ scores, tracks, top }) {
-  console.log({ top });
+  // console.log({ top });
   const data = [];
-  tracks.map(track => {
+  tracks.map((track) => {
     data.push({ track: track.name, score: scores[track.id] });
   });
 
-  //todo have multiple data arrays for each track, each bars
-  console.log({ scores });
+  // console.log({ scores });
   return (
     <div className="bargraph w-4/5 h-full m-10">
-      <div className="flex h-full">
-        <section className="y-axis flex-col-reverse w-auto border h-full">
-          <p>50</p>
-          <p>40</p>
-          <p>30</p>
-          <p>20</p>
-          <p>10</p>
-          <p>0</p>
-        </section>
+      <div className="flex h-56">
         <div className="flex w-full justify-around border border-red-400 border-solid ">
-          {Object.entries(scores).map(e => (
+          {Object.entries(scores).map((e) => (
             <div className=" w-full h-full rounded-lg m-auto border flex flex-col-reverse">
               <div
-                className="bg-blue h-full border-r border-solid border-pink rounded-lg duration-1000 ease-in-out"
+                className="bg-blue h-full border-r border-solid border-pink rounded-lg duration-1000 ease-in-out flex justify-center"
                 style={{
-                  height: `${e[1] * 4}%`,
-                  background: top.id == e[0] ? "lightblue" : "pink"
+                  height: `${e[1] * 3}%`,
+                  background: top.id == e[0] ? "lightblue" : "pink",
                 }}
-              />
+              >
+                {`${Math.round(e[1] * 3)}%`}
+              </div>
             </div>
           ))}
         </div>
       </div>
-      <section className="x-axis flex justify-between border w-full pl-8">
-        {tracks.map(track => (
-          <p>{track.name}</p>
+      <section className="x-axis flex justify-around border w-full pl-8 m-auto">
+        {tracks.map((track) => (
+          <p className={`w-1/${tracks.length} text-center`}>{track.name}</p>
         ))}
       </section>
     </div>
-    // <BarChart
-    //   width={1000}
-    //   height={300}
-    //   data={data}
-    //   margin={{
-    //     top: 5,
-    //     right: 30,
-    //     left: 20,
-    //     bottom: 5
-    //   }}
-    // >
-    //   <CartesianGrid strokeDasharray="3 3" />
-    //   <XAxis dataKey="track" />
-    //   <YAxis />
-    //   <Tooltip />
-    //   <Legend />
-    //   <Bar
-    //     dataKey="score"
-    //     fill="#f56565"
-    //     // onClick={evt => history.push(`/quiz/${evt.track}`)}
-    //   />
-    // </BarChart>
   );
 }
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     scores: state.scores,
-    tracks: state.tracks
+    tracks: state.tracks,
   };
 };
 export default withRouter(connect(mapStateToProps, {})(BarGraph));
