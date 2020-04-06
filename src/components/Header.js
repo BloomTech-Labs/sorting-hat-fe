1c514f0256fb71d894ee1ff73709e12694679afb
Jacob Prudent <jake.prudent@gmail.com>
2020-04-06 13:11:50 -0700

    Preped for  presentation
    Co-authored-by: Aaron Janovitch <aaronjan98@gmail.com>
    Co-authored-by: Arin Ramer <arinramer@gmail.com>
    Co-authored-by: Braden <fish0859@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,29 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 function Header() {
-  return (
-    <nav className="flex items-center justify-between flex-wrap bg-white p-6">
-      <NavLink to="/" className="font-bold text-xl text-red-500 mr-6">
-        Sorting Hat
-      </NavLink>
+	return (
+		<nav className="flex items-center justify-between flex-wrap bg-white p-6">
+			<NavLink to="/" className="font-bold text-xl text-red-500 mr-6">
+				Sorting Hat
+			</NavLink>
 
-      <div className="text-sm sm:flex-grow">
-        <NavLink
-          to=""
-          className="block mt-4 sm:inline-block sm:mt-0 text-black hover:text-red-500 mr-4"
-        >
-          about
-        </NavLink>
-        <NavLink
-          to="/quiz"
-          className="block mt-4 sm:inline-block sm:mt-0 text-black hover:text-red-500 mr-4"
-        >
-          start quiz
-        </NavLink>
-      </div>
-    </nav>
-  );
+			<div className="text-sm sm:flex-grow">
+				{/* <NavLink
+					to=""
+					className="block hidden mt-4 sm:inline-block sm:mt-0 text-black hover:text-red-500 mr-4 "
+				>
+					about
+				</NavLink> */}
+				<NavLink
+					to="/quiz"
+					className="block mt-4 sm:inline-block sm:mt-0 text-black hover:text-red-500 mr-4"
+				>
+					start quiz
+				</NavLink>
+			</div>
+		</nav>
+	);
 }
 
 export default Header;
