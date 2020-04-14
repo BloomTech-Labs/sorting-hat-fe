341a4d07a6314849aa42984b78c602b187b0b69e
Tomas Ortiz <TomasOrtiz@protonmail.com>
2020-04-14 16:27:17 -0700

    cleaned up some formatting and added more spacing to quiz and results.

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,240 +1,245 @@
-import React, {useState, useEffect} from 'react';
-import {Redirect} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+
 //* Redux
-import {connect} from 'react-redux';
-import {setScores} from '../redux/actions/setScores';
-import {setSelectedAnswers} from '../redux/actions/setSelectedAnswers';
+import { connect } from "react-redux";
+import { setScores } from "../redux/actions/setScores";
+import { setSelectedAnswers } from "../redux/actions/setSelectedAnswers";
+
 //* Components
-import ProgressBar from './ProgressBar';
-// import AnswerButton from './Button';
+import ProgressBar from "./ProgressBar";
+
 //* Checkbox Images
-import Checkbox from '../img/Checkbox.svg';
-import UnCheckbox from '../img/UnCheckbox.svg';
+import Checkbox from "../img/Checkbox.svg";
+import UnCheckbox from "../img/UnCheckbox.svg";
+
 //Arrow Images
-import ArrowPurpleL from '../img/ArrowPurpleL.svg';
-import ArrowWhiteL from '../img/ArrowWhiteL.svg';
-import ArrowWhiteR from '../img/ArrowWhiteR.svg';
-// import ArrowWhite from "../img/ArrowWhite.svg";
+import ArrowPurpleL from "../img/ArrowPurpleL.svg";
+import ArrowWhiteR from "../img/ArrowWhiteR.svg";
 
 function Quiz(props) {
-	//Setting State
-	const {
-		setScores,
-		questions,
-		answers,
-		points,
-		setSelectedAnswers,
-		questionAnswers,
-	} = props;
+  //Setting State
+  const {
+    setScores,
+    questions,
+    answers,
+    points,
+    setSelectedAnswers,
+    questionAnswers,
+  } = props;
 
-	//State of the Quiz
-	const [curQuesIndex, setCurQuesIndex] = useState(0);
-	const [selAnswer, setSelAnswer] = useState(false);
-	const [btnColor, setBtnColor] = useState('purple-400');
+  //State of the Quiz
+  const [curQuesIndex, setCurQuesIndex] = useState(0);
+  const [selAnswer, setSelAnswer] = useState(false);
+  const [btnColor, setBtnColor] = useState("purple-400");
 
-	//May need to create a cap on this later if too many questions
-	//are added to the back end.
-	const totalNumQues = questions.length;
+  //May need to create a cap on this later if too many questions
+  //are added to the back end.
+  const totalNumQues = questions.length;
 
-	/* why not initiate setScore with {} instead of this useEffect?? */
-	useEffect(() => {
-		setScores({});
-	}, []);
+  //   questions.length; slot this in to fix quiz instead of 1
 
-	if (!totalNumQues) {
-		return <Redirect to="/" />;
-	} else if (curQuesIndex === totalNumQues) {
-		// totalNumQues
-		//If the quiz is finished it will redirect Results component
+  /* why not initiate setScore with {} instead of this useEffect?? */
+  useEffect(() => {
+    setScores({});
+  }, []);
 
-		let totalScores = {};
-		questionAnswers.forEach((answer) => {
-			const currentPoints = points.filter(
-				(point) => point.answer_id === answer.id
-			);
-			for (const key in currentPoints) {
-				const ansTrackPoint = JSON.parse(currentPoints[key].points);
-				if (!totalScores[key]) {
-					totalScores[key] = 0;
-				}
-				totalScores = {
-					...totalScores,
-					[key]: totalScores[key] + ansTrackPoint,
-				};
-			}
-		});
-		setScores(totalScores);
-		return <Redirect to="/results" />;
-	}
+  if (!totalNumQues) {
+    return <Redirect to="/" />;
+  } else if (curQuesIndex === totalNumQues) {
+    // totalNumQues
+    //If the quiz is finished it will redirect Results component
 
-	//Scores are being updated based off of user selections
+    let totalScores = {};
+    questionAnswers.forEach((answer) => {
+      const currentPoints = points.filter(
+        (point) => point.answer_id === answer.id
+      );
+      for (const key in currentPoints) {
+        const ansTrackPoint = JSON.parse(currentPoints[key].points);
+        if (!totalScores[key]) {
+          totalScores[key] = 0;
+        }
+        totalScores = {
+          ...totalScores,
+          [key]: totalScores[key] + ansTrackPoint,
+        };
+      }
+    });
+    setScores(totalScores);
+    return <Redirect to="/results" />;
+  }
 
-	const updateSelAnswers = () => {
-		//This action updates the score based on points associated with the tracks
-		const existingNextAns = questionAnswers.find(
-			(answer) => answer.question_id === questions[curQuesIndex].id + 1
-		);
-		console.log({selAnswer});
-		setSelAnswer(existingNextAns ? existingNextAns : false);
-		// setBtnColor("purple-400");
-		if (selAnswer) {
-			//button background settings
-			setBtnColor('purple-900');
-			// Adds the selected answer points object to history
-			setCurQuesIndex(curQuesIndex + 1);
+  //Scores are being updated based off of user selections
 
-			const existingAnsIndex = questionAnswers.findIndex(
-				(answer) => answer.question_id === selAnswer.question_id
-			);
-			if (existingAnsIndex !== -1) {
-				questionAnswers.splice(existingAnsIndex, 1);
-			}
-			setSelectedAnswers([
-				...questionAnswers,
-				{
-					question_id: selAnswer.question_id,
-					id: selAnswer.id,
-				},
-			]);
-		}
-	};
+  const updateSelAnswers = () => {
+    //This action updates the score based on points associated with the tracks
+    const existingNextAns = questionAnswers.find(
+      (answer) => answer.question_id === questions[curQuesIndex].id + 1
+    );
+    console.log({ selAnswer });
+    setSelAnswer(existingNextAns ? existingNextAns : false);
+    // setBtnColor("purple-400");
+    if (selAnswer) {
+      //button background settings
+      setBtnColor("purple-900");
+      // Adds the selected answer points object to history
+      setCurQuesIndex(curQuesIndex + 1);
 
-	function handleBack() {
-		setCurQuesIndex(curQuesIndex - 1);
-		setBtnColor('purple-900');
-		setSelAnswer(
-			questionAnswers.find(
-				(answer) => answer.question_id + 1 === questions[curQuesIndex].id
-			)
-		);
-	}
-	//todo use like a useEffect, (not reRendering component)
-	function highlightButton(answerBtn) {
-		if (selAnswer && answerBtn && selAnswer.id === answerBtn.id) {
-			return 'purple-100';
-		} else {
-			return 'white';
-		}
-	}
+      const existingAnsIndex = questionAnswers.findIndex(
+        (answer) => answer.question_id === selAnswer.question_id
+      );
+      if (existingAnsIndex !== -1) {
+        questionAnswers.splice(existingAnsIndex, 1);
+      }
+      setSelectedAnswers([
+        ...questionAnswers,
+        {
+          question_id: selAnswer.question_id,
+          id: selAnswer.id,
+        },
+      ]);
+    }
+  };
 
-	//todo use like a useEffect, (not reRendering component)
-	function checkBoxButton(answerBtn, index) {
-		if (
-			(selAnswer && answerBtn && selAnswer.id === answerBtn.id) ||
-			selAnswer.id === index + 1
-		) {
-			return Checkbox;
-		} else {
-			return UnCheckbox;
-		}
-	}
+  function handleBack() {
+    setCurQuesIndex(curQuesIndex - 1);
+    setBtnColor("purple-900");
+    setSelAnswer(
+      questionAnswers.find(
+        (answer) => answer.question_id + 1 === questions[curQuesIndex].id
+      )
+    );
+  }
+  //todo use like a useEffect, (not reRendering component)
+  function highlightButton(answerBtn) {
+    if (selAnswer && answerBtn && selAnswer.id === answerBtn.id) {
+      return "purple-100";
+    } else {
+      return "white";
+    }
+  }
 
-	//* renders buttons for each answer
-	//Checkbox and index define relationship with the button and image
-	const answerButton = (answer, index) => (
-		<div
-			key={`div-${index}`}
-			className="flex items-center justify-start w-full mt-1 fira-sans"
-		>
-			{/* Changes based off of whether it is selected or not. 
+  //todo use like a useEffect, (not reRendering component)
+  function checkBoxButton(answerBtn, index) {
+    if (
+      (selAnswer && answerBtn && selAnswer.id === answerBtn.id) ||
+      selAnswer.id === index + 1
+    ) {
+      return Checkbox;
+    } else {
+      return UnCheckbox;
+    }
+  }
+
+  //* renders buttons for each answer
+  //Checkbox and index define relationship with the button and image
+  const answerButton = (answer, index) => (
+    <div
+      key={`div-${index}`}
+      className="flex items-center justify-start w-full mt-1 fira-sans mb-2"
+    >
+      {/* Changes based off of whether it is selected or not. 
 			Checkbox contains the id of the current selected answer.
 			If the current question matches the id stored as checkbox, a 
 			checkbox is displayed.
 			*/}
-			<img src={checkBoxButton(answer, index)} alt="checkbox" />
-			<button
-				key={index}
-				onClick={() => {
-					setSelAnswer(answer);
-					setBtnColor('purple-900');
-				}}
-				className={`fira-sans mark w-full p-1 ml-2 text-left hover:bg-purple-100 bg-${highlightButton(
-					answer,
-					index
-				)}`}
-			>
-				{answer.choice}
-			</button>
-		</div>
-	);
+      <img src={checkBoxButton(answer, index)} alt="checkbox" />
+      <button
+        key={index}
+        onClick={() => {
+          setSelAnswer(answer);
+          setBtnColor("purple-900");
+        }}
+        className={`fira-sans mark w-full p-1 ml-2 text-left hover:bg-purple-100 bg-${highlightButton(
+          answer,
+          index
+        )}`}
+      >
+        {answer.choice}
+      </button>
+    </div>
+  );
 
-	return (
-		<div className="w-1/2 h-screen p-1 m-auto mt-3">
-			{/*Current Question number*/}
-			<h1 className="pt-4 mt-2 text-xl text-3xl fira-sans">
-				Question {curQuesIndex + 1}
-			</h1>
+  return (
+    <div className="w-1/2 h-screen p-1 m-auto mt-3">
+      {/*Current Question number*/}
+      <h1 className="pt-4 mt-2 text-xl text-4xl fira-sans">
+        Question {curQuesIndex + 1}
+      </h1>
 
-			<ProgressBar progress={questions[curQuesIndex].id / totalNumQues} />
-			{/*Question Text*/}
-			<h1 className="mt-2 protoGray fira-sans">
-				{questions[curQuesIndex].question}
-			</h1>
-			{/*Answers to the Questions*/}
-			<div className="flex flex-col w-full mt-4 mb-2">
-				{answers.map(
-					(answer, index) =>
-						answer.question_id === questions[curQuesIndex].id &&
-						answerButton(answer, index)
-				)}
-			</div>
-			<div className="flex items-center justify-between m-auto mt-8 questrial actions">
-				{/*Back Button
+      <ProgressBar progress={questions[curQuesIndex].id / totalNumQues} />
+      {/*Question Text*/}
+      <h1 className="mt-2 protoGray fira-sans text-2xl">
+        {questions[curQuesIndex].question}
+      </h1>
+      {/*Answers to the Questions*/}
+      <div className="flex flex-col w-full mt-4 mb-2 text-lg">
+        {answers.map(
+          (answer, index) =>
+            answer.question_id === questions[curQuesIndex].id &&
+            answerButton(answer, index)
+        )}
+      </div>
+      <div className="flex items-center justify-between m-auto mt-8 questrial actions">
+        {/*Back Button
         
           If it isn't the first question, the back button says back and updates history.
            If it is the first question it redirects the user to landing page
         */}
-				{curQuesIndex > 0 ? (
-					<button
-						onClick={handleBack}
-						className={`questrial border-2 hover:bg-purple-100 p-2 px-4 border-purple-100 flex rounded-lg items-center`}
-					>
-						<img
-							src={ArrowPurpleL}
-							alt="leftArrow"
-							size="1.3rem"
-							className="pr-4"
-						/>{' '}
-						<span className="text-purple-100">Back</span>
-					</button>
-				) : (
-					<button
-						onClick={() => props.history.push('/')}
-						className={`questrial border-2 hover:bg-purple-100 p-2 px-4 border-purple-100 flex rounded-lg items-center`}
-					>
-						<img
-							src={ArrowPurpleL}
-							alt="leftArrow"
-							size="1.3rem"
-							className="pr-4 "
-						/>{' '}
-						<span className="text-purple-100">Home</span>
-					</button>
-				)}
-				<span className="text-gray-600">{`${questions[curQuesIndex].id}/${totalNumQues}`}</span>
+        {curQuesIndex > 0 ? (
+          <button
+            onClick={handleBack}
+            className={`questrial border-2 hover:bg-purple-100 p-2 px-4 border-purple-100 flex rounded-lg items-center`}
+          >
+            <img
+              src={ArrowPurpleL}
+              alt="leftArrow"
+              size="1.3rem"
+              className="pr-4"
+            />
+            <span className="text-purple-100 text-lg">Back</span>
+          </button>
+        ) : (
+          <button
+            onClick={() => props.history.push("/")}
+            className={`questrial border-2 hover:bg-purple-100 p-2 px-4 border-purple-100 flex rounded-lg items-center`}
+          >
+            <img
+              src={ArrowPurpleL}
+              alt="leftArrow"
+              size="1.3rem"
+              className="pr-4 "
+            />
+            <span className="text-purple-100">Home</span>
+          </button>
+        )}
+        <span className="text-gray-600">{`${questions[curQuesIndex].id}/${totalNumQues}`}</span>
 
-				{/*Next Button*/}
-				<button
-					onClick={updateSelAnswers}
-					className={`questrial flex bg-${btnColor} hover:bg-purple-100  p-2 px-4 rounded-lg items-center`}
-				>
-					<span className="pr-4 text-white">Next</span>
-					<img src={ArrowWhiteR} alt="rightArrow" size="1.3rem" />
-				</button>
-			</div>
-		</div>
-	);
+        {/*Next Button*/}
+        <button
+          onClick={updateSelAnswers}
+          className={`questrial flex bg-${btnColor} hover:bg-purple-100  p-2 px-4 rounded-lg items-center`}
+        >
+          <span className="pr-4 text-white">Next</span>
+          <img src={ArrowWhiteR} alt="rightArrow" size="1.3rem" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 //Redux interface
 
 const mapStateToProps = (state) => {
-	return {
-		questions: state.questions,
-		answers: state.answers,
-		questionAnswers: state.questionAnswers,
-		points: state.points,
-	};
+  return {
+    questions: state.questions,
+    answers: state.answers,
+    questionAnswers: state.questionAnswers,
+    points: state.points,
+  };
 };
 
-export default connect(mapStateToProps, {setScores, setSelectedAnswers})(Quiz);
+export default connect(mapStateToProps, { setScores, setSelectedAnswers })(
+  Quiz
+);
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,159 +1,160 @@
-import React, {useState, useEffect} from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
-import ArrowWhiteR from '../img/ArrowWhiteR.svg';
-import ArrowPurpleL from '../img/ArrowPurpleL.svg';
-import BarGraph from './BarGraph';
-import {setSelectedAnswers} from '../redux/actions/setSelectedAnswers';
+import React, { useState, useEffect } from "react";
+import { Link, Redirect } from "react-router-dom";
+
+// Redux
+import { connect } from "react-redux";
+
+//Arrow Images
+import ArrowWhiteR from "../img/ArrowWhiteR.svg";
+import ArrowPurpleL from "../img/ArrowPurpleL.svg";
+
+// BarGraph
+import BarGraph from "./BarGraph";
+import { setSelectedAnswers } from "../redux/actions/setSelectedAnswers";
 //We need to have the endpoints from the backend
 
-function Results({scores, tracks, setSelectedAnswers}) {
-	const [selectedTrack, setSelectedTrack] = useState({});
+function Results({ scores, tracks, setSelectedAnswers }) {
+  const [selectedTrack, setSelectedTrack] = useState({});
 
-	useEffect(() => {
-		let highest = {track: null, score: 0};
-		for (let score in scores) {
-			if (scores[score] > highest.score) {
-				highest = {track: score, score: scores[score]};
-			}
-		}
-		setSelectedTrack(tracks.find((track) => track.id == highest.track));
-		setSelectedAnswers([]);
-	}, []);
+  useEffect(() => {
+    let highest = { track: null, score: 0 };
+    for (let score in scores) {
+      if (scores[score] > highest.score) {
+        highest = { track: score, score: scores[score] };
+      }
+    }
+    setSelectedTrack(tracks.find((track) => track.id == highest.track));
+    setSelectedAnswers([]);
+  }, []);
 
-	if (!tracks.length) {
-		return <Redirect to="/" />;
-	}
-	return (
-		<>
-			{/*Results Body*/}
-			<div className="flex items-center justify-center noto-sans">
-				<section className="max-w-3xl m-10 border-black noto-sans flex-column">
-					<h2 className="text-3xl font-bold text-black border-b-2 ">Results</h2>
-					<div className="">
-						<p className="py-3 protoGray noto-sans">
-							We sorted you into these categories with your primary strengths in
-							{'  '} {selectedTrack.name}. Take this into consideration all
-							tracks and percentages, and remember this is just a quiz. Follow
-							your heart.
-						</p>
-					</div>
-					<div className="flex justify-center text-lg questrial flex-column">
-						<BarGraph top={selectedTrack} setTrack={setSelectedTrack} />
-					</div>
-					<div>
-						<h2 className="text-3xl border-b-2 fira-sans">
-							{selectedTrack.name}
-						</h2>
-						<p className="py-3 protoGray noto-sans">
-							{selectedTrack.description}
-						</p>
-					</div>
+  if (!tracks.length) {
+    return <Redirect to="/" />;
+  }
+  return (
+    <>
+      {/*Results Body*/}
+      <div className="flex items-center justify-center noto-sans">
+        <section className="max-w-3xl m-10 border-black noto-sans flex-column">
+          <h2 className="pb-1 text-3xl font-bold text-black border-b-2 ">
+            Results
+          </h2>
+          <div>
+            <p className="py-3 protoGray noto-sans">
+              We sorted you into these categories with your primary strengths in
+              <span className="font-bold">
+                {"  "} {selectedTrack.name}
+              </span>
+              . Take this into consideration all tracks and percentages, and
+              remember this is just a quiz. Follow your heart.
+            </p>
+          </div>
+          <div className="flex justify-center text-lg questrial flex-column mb-16">
+            <BarGraph top={selectedTrack} setTrack={setSelectedTrack} />
+          </div>
+          <div>
+            <h2 className="pb-1 text-3xl border-b-2 fira-sans">
+              {selectedTrack.name}
+            </h2>
+            <p className="py-3 protoGray noto-sans mb-20">
+              {selectedTrack.description}
+            </p>
+          </div>
 
-					{/* Selected Track Name Traits */}
-					<div className="container mt-5">
-						<h2 className="text-3xl border-b-2 fira-sans">
-							{'  '} {selectedTrack.name} Traits
-						</h2>
-						{/* This View Courses button needs to be inline with the H2 above ↑ */}
-						{/* <div className="inline-flex px-1 py-1 pt-2 mx-5 my-8">
-							<Link
-								to="/courses"
-								className="flex bg-purple-900 hover:bg-purple-100 text-white mr-5 py-0.5 px-10 border border-purple-900 rounded align-baseline"
-							>
-								<i className="material-icons">arrow_left</i>
-								<span className="questrial">View Courses</span>
-							</Link>
-						</div> */}
+          {/* Selected Track Name Traits */}
+          <div className="container mt-5">
+            <h2 className="pb-1 text-3xl border-b-2 fira-sans">
+              {"  "} {selectedTrack.name} Traits
+            </h2>
+            {/* This View Courses button needs to be inline with the H2 above ↑ */}
 
-						<p className="py-3 protoGray noto-sans">
-							{selectedTrack.strengths}
-						</p>
-					</div>
-					<div className="flex-col items-center justify-center noto-sans">
-						<h2 className="self-start text-3xl fira-sans">Learn More</h2>
-						<div className="flex justify-center py-2">
-							<iframe
-								width="100%"
-								height="315"
-								src={selectedTrack.link}
-								frameBorder="0"
-								allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-								allowFullScreen
-							></iframe>
-							{/* {selectedTrack. ? trackInfo.trackVideo : } */}
-						</div>
-					</div>
-					<div>
-						<h2 className="mb-2 text-3xl text-black border-b-2 fira-sans">
-							Discover Other Tracks
-						</h2>
-						{/*I need to axios all this info*/}
-						{tracks.map((el) => {
-							if (el.id != selectedTrack.id) {
-								return (
-									<div
-										key={el.id}
-										className="py-1 resultsLinkColor"
-										onClick={() => setSelectedTrack(el)}
-									>
-										{el.name}
-										<span className="font-normal cursor-pointer resultsLinkColor">
-											{' '}
-											- {el.shortDesc}
-										</span>
-									</div>
-								);
-							}
-						})}
-					</div>
+            <p className="py-3 protoGray noto-sans mb-20">
+              {selectedTrack.strengths}
+            </p>
+          </div>
+          <div className="flex-col items-center justify-center noto-sans mb-10">
+            <h2 className="pb-1 self-start text-3xl fira-sans">Learn More</h2>
+            <div className="flex justify-center py-2 mb-20">
+              <iframe
+                width="100%"
+                height="315"
+                src={selectedTrack.link}
+                frameBorder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+              {/* {selectedTrack. ? trackInfo.trackVideo : } */}
+            </div>
+          </div>
+          <div>
+            <h2 className="pb-1 mb-2 text-3xl text-black border-b-2 fira-sans">
+              Discover Other Tracks
+            </h2>
+            {/*I need to axios all this info*/}
+            {tracks.map((el) => {
+              if (el.id != selectedTrack.id) {
+                return (
+                  <div
+                    key={el.id}
+                    className="py-3 resultsLinkColor"
+                    onClick={() => setSelectedTrack(el)}
+                  >
+                    {el.name}
+                    <span className="font-normal cursor-pointer resultsLinkColor">
+                      {" "}
+                      - {el.shortDesc}
+                    </span>
+                  </div>
+                );
+              }
+            })}
+          </div>
 
-					{/* Bottom Buttons Section ↓ */}
+          {/* Bottom Buttons Section ↓ */}
 
-					<section className="flex flex-row justify-between mt-20">
-						<div className="flex justify-start py-1 pt-2 pr-1 my-8 mr-5 ">
-							<Link
-								to="/quiz"
-								className={`border-2 border-purple-100 hover:border-purple-900 flex p-2 px-8 rounded-lg justify-center items-center btnRound`}
-							>
-								<img
-									src={ArrowPurpleL}
-									alt="leftArrow"
-									size="1.3rem"
-									className="pr-4 m-1"
-								/>{' '}
-								<span className="text-purple-100 questrial">Retake Quiz</span>
-							</Link>
-						</div>
-						<div className="flex justify-end py-1 pt-2 pl-1 my-8 ml-5">
-							<Link
-								to="/courses"
-								className="flex align-baseline justify-between bg-purple-900 hover:bg-purple-100 text-white py-0.5 px-4 border border-purple-900 rounded btnRound "
-							>
-								<span className="flex items-center justify-end questrial">
-									View Courses
-								</span>
-								<img
-									src={ArrowWhiteR}
-									alt="rightArrow"
-									size="1.3rem"
-									className="pl-4 m-1"
-								/>{' '}
-							</Link>
-						</div>
-					</section>
-				</section>
-			</div>
-			{/*Bottom Nav Bar */}
-		</>
-	);
+          <section className="flex flex-row justify-between mt-20">
+            <div className="flex justify-start py-1 pt-2 pr-1 my-8 mr-5 ">
+              <Link
+                to="/quiz"
+                className={`border-2 border-purple-100 hover:border-purple-900 flex p-2 px-8 rounded-lg justify-center items-center btnRound`}
+              >
+                <img
+                  src={ArrowPurpleL}
+                  alt="leftArrow"
+                  size="1.3rem"
+                  className="pr-4 m-1"
+                />
+                <span className="text-purple-100 questrial">Retake Quiz</span>
+              </Link>
+            </div>
+            <div className="flex justify-end py-1 pt-2 pl-1 my-8 ml-5">
+              <Link
+                to="/courses"
+                className="flex align-baseline justify-between bg-purple-900 hover:bg-purple-100 text-white py-0.5 px-4 border border-purple-900 rounded btnRound "
+              >
+                <span className="flex items-center justify-end questrial">
+                  View Courses
+                </span>
+                <img
+                  src={ArrowWhiteR}
+                  alt="rightArrow"
+                  size="1.3rem"
+                  className="pl-4 m-1"
+                />{" "}
+              </Link>
+            </div>
+          </section>
+        </section>
+      </div>
+      {/*Bottom Nav Bar */}
+    </>
+  );
 }
 
 //Redux Interface
 const mapStateToProps = (state) => {
-	return {
-		scores: state.scores,
-		tracks: state.tracks,
-	};
+  return {
+    scores: state.scores,
+    tracks: state.tracks,
+  };
 };
-export default connect(mapStateToProps, {setSelectedAnswers})(Results);
+export default connect(mapStateToProps, { setSelectedAnswers })(Results);
