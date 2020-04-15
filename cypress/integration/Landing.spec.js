b2d14d9a197e9fdf087784072ad24e0c2b29a1a1
aaronjan <aaronjan98@gmail.com>
2020-04-15 09:34:22 -0700

    updated Landing.spec.js
    updated test: 'Button onclick should work' to check if the answers were rendering
    
    Co-Authored-By: Braden <1sdc0d3r@users.noreply.github.com>
    Co-Authored-By: Tomas Ortiz <tomasortiz@protonmail.com>
    Co-Authored-By: Jacob Prudent <prudentj@users.noreply.github.com>
    Co-Authored-By: Arin Ramer <arinramer@gmail.com>

---
+++ cypress/integration/Landing.spec.js
@@ -0,0 +1,22 @@
+describe('Render Landing Page', () => {
+	beforeEach(() => {
+		cy.visit('/');
+	});
+
+	it('finds the appropriate elements for Landing Page', () => {
+		// cy.get('h2').should('contian', 'Discover the Tech Career for You');
+		cy.contains('Discover the Tech Career for You');
+		cy.contains(
+			'Take our 5 minute survey to discover which tech field would be right for you. Discover the opportunity at your fingertips.'
+		);
+		cy.contains('Start Quiz');
+	});
+
+	it('Button onclick should work', () => {
+		cy.get('.z-10.w-full').click();
+		cy.contains('Question 1');
+		// cy.get('div[class=w-full]').click();
+		cy.get('div[class="flex flex-col w-full mt-4 mb-2"]').should('have.length', 1);
+		cy.get('div[class="flex items-center justify-start w-full mt-1 fira-sans"]').should('have.length', 4);
+	});
+});
