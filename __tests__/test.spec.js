4823b7b126924fd2a65fe9f658745893bc40677a
Braden <fish2355@hotmail.com>
2020-04-27 13:57:52 -0600

    testing test test

--- __tests__/test.spec.js
+++ __tests__/test.spec.js
@@ -1,3 +1,18 @@
-it("comes out as true", () => {
-  expect(true).toBe(true);
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { Landing } from "../src/components/Landing";
+
+afterEach(rtl.cleanup);
+
+describe("<Landing />", () => {
+  it("renders without crashing", () => {
+    const wrapper = rtl.render(<Landing />);
+    wrapper.getByText(/about/i);
+  });
+
+  // it("start Btn has 'Start Quiz'", () => {
+  //   const { getByText } = rtl.render(<Landing />);
+  //   expect(getByText(/start quiz/i)).toHaveTextContent(/start quiz/i);
+  // });
 });
