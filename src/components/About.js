27e50b3b37cb83170962fea41d8978f496bcea36
Jacob Prudent <jake.prudent@gmail.com>
2020-04-28 22:42:01 -0700

    About page added images

--- src/components/About.js
+++ src/components/About.js
@@ -7,55 +7,71 @@ import styled from "styled-components";
 import ArrowPurpleL from "../img/ArrowPurpleL.svg";
 import ArrowWhiteR from "../img/ArrowWhiteR.svg";
 import { ViewCoursesBtn, TakeQuizBtn } from "./buttons/styledButtons";
+import AboutImage1 from "../img/AboutImage1.svg";
+import AboutImage2 from "../img/AboutImage2.svg";
+import AboutImage3 from "../img/AboutImage3.svg";
 
 const About = (props) => {
+  //Tailwind styles for containers at texttop
+  const textCont = "w-full lg:w-1/2";
+  const imgStyles = "hidden lg:block lg:w-1/2";
+
   const { history } = props;
   return (
     <div>
       {history.location.pathname !== "/" ? <Header /> : <></>}
       <div className="flex items-center justify-center mr-8 ml-8 mt-20">
-        <div className="flex flex-col m-4 max-w-3xl">
-          <section className=" mt-4 mb-4">
-            <h2 className="mb-1 text-2xl lg:text-5xl font-bold text-black border-b-2 open-sans">
-              About
+        <div className="flex flex-col m-4 mt-10 max-w-3xl">
+          <section className=" mt-4 mb-4 flex">
+            <div className={textCont}>
+              <h2 className="mb-1 text-2xl lg:text-5xl font-bold text-black border-b-2 open-sans">
+                About
             </h2>
-            {/* There needs to be more text here below! */}
-            <p className="flex-col items-center justify-center text-base lg:text-lg mt-4 pb-24 leading-loose protoGray open-sans">
-              Tech Sorting Hat was inspired and built by tech students. We aim
-              to have a fun and informative quiz to help potential tech students
-              make a decision about their future careers.
+              {/* There needs to be more text here below! */}
+              <p className="flex-col items-center justify-center text-base lg:text-lg mt-4 pb-24 leading-loose protoGray open-sans">
+                Tech Sorting Hat was inspired and built by tech students. We aim
+                to have a fun and informative quiz to help potential tech students
+                make a decision about their future careers.
             </p>
+            </div>
+            <img className={imgStyles} src={AboutImage1} />
           </section>
 
-          <section className="">
-            <h2 className="mb-1 text-2xl lg:text-5xl text-black border-b-2 fira-sans  ">
-              Philosophy
+          <section className="flex">
+            <img className={imgStyles} src={AboutImage2} />
+            <div className={textCont}>
+              <h2 className="mb-1 text-2xl lg:text-5xl text-black border-b-2 fira-sans  ">
+                Philosophy
             </h2>
-            <p className="flex-col items-center justify-center text-base lg:text-lg mt-4 pb-24 leading-loose protoGray open-sans">
-              We wanted to showcase the different industries in the tech field
-              for people who are seeking an informed decision on which
-              subsection to choose. We threw away technical jargon in order to
-              show empathy to those outside the industry, and to quiz them based
-              on their personality, their technical capabilities. All people are
-              capable of pursuing a career in the tech field, and it is our job
-              to help find their niche based on their personality.
+              <p className="flex-col items-center justify-center text-base lg:text-lg mt-4 pb-24 leading-loose protoGray open-sans">
+                We wanted to showcase the different industries in the tech field
+                for people who are seeking an informed decision on which
+                subsection to choose. We threw away technical jargon in order to
+                show empathy to those outside the industry, and to quiz them based
+                on their personality, their technical capabilities. All people are
+                capable of pursuing a career in the tech field, and it is our job
+                to help find their niche based on their personality.
             </p>
+            </div>
           </section>
 
-          <section className="container mt-5 ">
-            <h2 className="mb-1 text-2xl lg:text-5xl text-black border-b-2 fira-sans  ">
-              How
-            </h2>
-            <p className="flex-col items-center justify-center text-base lg:text-lg mt-4 leading-loose protoGray open-sans">
-              We surveyed 70 current tech students, interviewed 13 tech
-              students, and 2 tech instructors. We discovered that all tracks in
-              the tech field are extremely similar, but subtle differences
-              between each emerged from analyzing the data. We created questions
-              that do not deal with tech, but relate to how people in different
-              tech fields think. Keep in mind, anyone has the capability to
-              succeed in any career, and this quiz should not hinder someones
-              passion for a particular tech field.
-            </p>
+          <section className=" flex container mt-5 ">
+            <div className={textCont}>
+              <h2 className="mb-1 text-2xl lg:text-5xl text-black border-b-2 fira-sans  ">
+                How
+              </h2>
+              <p className="flex-col items-center justify-center text-base lg:text-lg mt-4 leading-loose protoGray open-sans">
+                We surveyed 70 current tech students, interviewed 13 tech
+                students, and 2 tech instructors. We discovered that all tracks in
+                the tech field are extremely similar, but subtle differences
+                between each emerged from analyzing the data. We created questions
+                that do not deal with tech, but relate to how people in different
+                tech fields think. Keep in mind, anyone has the capability to
+                succeed in any career, and this quiz should not hinder someones
+                passion for a particular tech field.
+              </p>
+            </div>
+            <img className={imgStyles} src={AboutImage3} />
           </section>
 
           <section className="flex flex-row justify-between  pt- 16 lg:pt-32">
