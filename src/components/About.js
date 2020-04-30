b186632b5024b8ef6b8ab5a2487e8035a29af92a
aaronjan <aaronjan98@gmail.com>
2020-04-30 14:51:09 -0700

    Update About.js
    changed positioning of text and about svg images
    
    Co-Authored-By: Arin Ramer <arinramer@gmail.com>
    Co-Authored-By: Jacob Prudent <prudentj@users.noreply.github.com>
    Co-Authored-By: Tomas Ortiz <tomasortiz@protonmail.com>
    Co-Authored-By: Braden <1sdc0d3r@users.noreply.github.com>

--- src/components/About.js
+++ src/components/About.js
@@ -13,61 +13,61 @@ import AboutImage3 from "../img/AboutImage3.svg";
 
 const About = (props) => {
   //Tailwind styles for containers at texttop
-  const textCont = "w-full lg:w-1/2 ";
-  const imgStyles = "hidden lg:block h-full lg:w-1/2 ";
+  const textCont = "w-full lg:w-1/2 p-8";
+  const imgStyles = "hidden lg:block h-full lg:w-1/2";
   const { history } = props;
   return (
     <div>
       {history.location.pathname !== "/" ? <Header /> : <></>}
       <div className="flex items-center justify-center mr-8 ml-8 mt-20">
         <div className="flex flex-col m-4 mt-10 max-w-3xl ">
-          <section className=" mt-4 mb-4 flex justify-center items-center">
+          <section className=" mt-4 mb-4 flex justify-between items-center">
             <div className={textCont}>
-              <h2 className="mb-1 text-2xl lg:text-3xl font-bold text-black border-b-2 open-sans">
+              <h2 className="mb-1 text-2xl lg:text-3xl font-bold text-black border-b-2 open-sans lg:font-normal">
                 About
               </h2>
               {/* There needs to be more text here below! */}
-              <div className="flex-col items-center justify-center text-base lg:text-lg mt-4 pb-24 leading-loose protoGray open-sans">
-                <p className="py-1">Tech Sorting Hat was inspired and built by tech students.</p>
-                <p className="py-1">We aim to have a fun quiz that informs users about tech careers.</p>
-                <p className="py-1">Our hope is to spark an interest the interest.</p>
+              <div className="flex-col items-center justify-center text-base lg:text-lg mt-4 pb-24 lg:pb-0 leading-loose protoGray open-sans">
+                <p className="pb-4">Tech Sorting Hat was inspired and built by tech students.</p>
+                <p className="pb-4">We aim to have a fun quiz that informs users about tech careers.</p>
+                <p className="pb-4">Our hope is to spark an interest the interest.</p>
               </div>
             </div>
             <img className={imgStyles} src={AboutImage1} />
           </section>
 
-          <section className="flex justify-center items-center">
-            <img className={imgStyles} src={AboutImage2} />
+          <section className="flex justify-between items-center">
+            <img className={imgStyles} src={AboutImage2}  />
             <div className={textCont}>
               <h2 className="mb-1 text-2xl lg:text-3xl text-black border-b-2 fira-sans  ">
                 Why it was created
             </h2>
-              <div className="flex-col items-center justify-center text-base lg:text-lg mt-4 pb-24 leading-loose protoGray open-sans">
-              <p>We wanted to provide information about tech careers in a way that empathizes with people outside the industry. No tech jargon.</p>
+              <div className="flex-col items-center justify-center text-base lg:text-lg mt-4 pb-24 lg:pb-0 leading-loose protoGray open-sans">
+              <p className="pb-4">We wanted to provide information about tech careers in a way that empathizes with people outside the industry. No tech jargon.</p>
               
-              <p>Our goal is to inspire, not discourage. Questions were carefully created to do this. We quizzed them based on personality, not what they knew about the tech industry.</p>
+              <p className="pb-4">Our goal is to inspire, not discourage. Questions were carefully created to do this. We quizzed them based on personality, not what they knew about the tech industry.</p>
               
-              <p>Anyone is capable of pursuing a career in the tech field, and it is our job to help find their niche based on their personality.</p>
+              <p className="pb-4">Anyone is capable of pursuing a career in the tech field, and it is our job to help find their niche based on their personality.</p>
             </div>
             </div>
           </section>
 
-          <section className=" flex container mt-5 justify-center items-center">
+          <section className=" flex container mt-5 justify-between items-center">
             <div className={textCont}>
-              <h2 className="mb-1 text-2xl lg:text-3xl text-black border-b-2 fira-sans font-bold ">
+              <h2 className="mb-1 text-2xl lg:text-3xl text-black border-b-2 fira-sans">
                 How it was made
               </h2>
               <p className="flex-col items-center justify-center text-base lg:text-lg mt-4 leading-loose protoGray open-sans">
-              We surveyed 70 current tech students, interviewed 13 tech students, and 2 tech instructors. 
-              All tracks in the tech field are extremely similar, but subtle differences between each emerged from analyzing the data.
-              We created questions that do not deal with tech, but relate to how people in different tech fields think. 
+              <p className="pb-4">We surveyed 70 current tech students, interviewed 13 tech students, and 2 tech instructors.</p>
+              <p className="pb-4">All tracks in the tech field are extremely similar, but subtle differences between each emerged from analyzing the data.</p>
+              <p className="pb-4">We created questions that do not deal with tech, but relate to how people in different tech fields think.</p>
               </p>
             </div>
             <img className={imgStyles} src={AboutImage3} />
           </section>
 
           <section className="flex flex-row justify-between  pt- 16 lg:pt-32">
-            <div className="flex justify-start py-1 pt-2 pr-1 my-8 mr-5 ">
+            <div className="flex justify-start pb-1 pt-2 pr-1 my-8 mr-5 ">
               <button
                 onClick={() => history.push("/quiz")}
                 //className={`border-2 border-purple-100 hover:border-primary flex p-2 px-6 rounded-lg justify-center items-center`}
